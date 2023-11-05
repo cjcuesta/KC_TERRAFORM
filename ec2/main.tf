0cad2e163d5c72383c71b614222d600cc150c1e4
Carlos Julio <cjcuesta@gmail.com>
2023-11-05 07:17:31 -0500

    Modificación del main.tf para que tome el tipo de instancia de los valores por defecto del archivo variables.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "basic-instance" {
   #ami = "ami-07a877f081af74359"
   ami           = data.aws_ami.amazon_linux.id # id del data consultado
-  instance_type = "t2.micro"                   # tipo de instancia 
+  instance_type = var.instance_type           # tipo de instancia 
 
   tags = {
     Name = "kc-terraform-ec2-carlos" # tag de la intancia  
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,6 +1,6 @@
 variable "instance_type" {
-    description = "EC2 instance Type"
-    type = string
-    default = "t2.micro"
-  
+  description = "EC2 instance Type"
+  type        = string
+  default     = "t2.micro"
+
 }
