3c42b30fb467ce7cf2ad0c680d015e619ef6bf83
Carlos Julio <cjcuesta@gmail.com>
2023-11-05 16:50:16 -0500

    Se agrega el provider TLS y su alias

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -4,10 +4,20 @@ terraform {
       source  = "hashicorp/aws" #el proveedor AWS
       version = "~> 5.0"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
 
 provider "aws" {
   region = "eu-west-1" #la region de Irlanda
+
+}
+
+provider "tls" {
+  alias = "private_key"
 
 }
