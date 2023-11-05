2f1942a22b806b122b8d4cf185d491c9cc2ea0a3
Carlos Julio <cjcuesta@gmail.com>
2023-11-05 06:21:31 -0500

    Se crea la instancia y se le coloca el tag

---
+++ ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "basic-instance" {
+  #ami = "ami-07a877f081af74359"
+  ami           = data.aws_ami.amazon_linux.id  # id del data consultado
+  instance_type = "t2.micro"                    # tipo de instancia 
+
+  tags = {
+    Name = "kc-terraform-ec2-carlos"            # tag de la intancia  
+  }
+
+}
