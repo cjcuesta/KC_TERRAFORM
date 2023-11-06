301668129397026706f6091796e3045801ade8f8
Carlos Julio <cjcuesta@gmail.com>
2023-11-05 19:50:48 -0500

    Se asocia el security group a la instancia

--- ec2/main.tf
+++ ec2/main.tf
@@ -58,12 +58,13 @@ resource "aws_security_group_rule" "kc_http" {
 
 resource "aws_instance" "basic-instance" {
   #ami = "ami-0a1f6cc8163bdcc75"
-  ami           = data.aws_ami.amazon_linux.id        # id del data consultado
-  instance_type = var.instance_type                   # tipo de instancia 
-  key_name      = aws_key_pair.kc_public_key.key_name #Asociamos la key_pair o llave publica
+  ami                    = data.aws_ami.amazon_linux.id        # id del data consultado
+  instance_type          = var.instance_type                   # tipo de instancia 
+  key_name               = aws_key_pair.kc_public_key.key_name #Asociamos la key_pair o llave publica
+  vpc_security_group_ids = [aws_security_group.kc_sg.id]
 
   tags = {
-    Name = "kc-terraform-ec2-carlos" # tag de la intancia  
+    Name = "kc-terraform-ec2-carlos" # tag de la instancia  
   }
 
 }
