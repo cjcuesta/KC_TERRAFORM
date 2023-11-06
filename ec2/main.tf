335ab4dc637ab28c1f849be944fd49cdec81f05e
Carlos Julio <cjcuesta@gmail.com>
2023-11-05 19:05:03 -0500

    Se adiciona el la regla para salida de todos los puertos 0 y todos los protocolos -1

--- ec2/main.tf
+++ ec2/main.tf
@@ -32,6 +32,17 @@ resource "aws_security_group_rule" "kc_ssh" {
 
 }
 
+resource "aws_security_group_rule" "kc_egress" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.kc_sg.id
+
+}
+
+
 
 
 resource "aws_instance" "basic-instance" {
